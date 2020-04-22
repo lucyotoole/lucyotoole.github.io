a3a54357467f614818a2c9e77aa738f419f718fa
lucyotoole <49875171+lucyotoole@users.noreply.github.com>
2020-04-22 12:17:23 -0400

    Create someone_is_trying_to_get_izzys_present.php

---
+++ someone_is_trying_to_get_izzys_present.php
@@ -0,0 +1,11 @@
+<!--created 04-22-2020-->
+<html>
+  <?php
+    if (echo $_GET["question"]; == "izzy"){
+      echo "Happy birthday!";
+    } else {
+      echo "Sorry you must not be Izzy, she would have know the answer!";
+    }
+    
+  ?>
+</html>
